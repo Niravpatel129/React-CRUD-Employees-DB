8d6c30cd66bc713359a7c1e4242219293cacf7a0
Niravpatel129 <niravpatelp129@gmail.com>
2019-12-05 12:48:12 -0500

    refactored apiUrlGetter

--- src/components/App/helpers/apiUrlGetter.js
+++ src/components/App/helpers/apiUrlGetter.js
@@ -1,15 +1,9 @@
 function getApi(params) {
-  console.log(params);
-  let apiURL;
-  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    // dev code
-    apiURL = 'http://localhost:8080' + params;
-    console.log('dev environment');
-  } else {
-    // production code
-    apiURL = 'https://backend-api-crud-app.herokuapp.com' + params;
-    console.log('production environment');
-  }
+  const apiURL =
+    !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+      ? `http://localhost:8080${params}`
+      : `https://backend-api-crud-app.herokuapp.com${params}`;
+
   return apiURL;
 }
 
