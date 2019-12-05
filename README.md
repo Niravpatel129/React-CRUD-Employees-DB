22a5c956129fbfd52a49347c9d752849d9ab9d6f
Niravpatel129 <niravpatelp129@gmail.com>
2019-12-05 15:40:25 -0500

    remove gitignore files

---
+++ README.md
@@ -0,0 +1,31 @@
+# Plexxis Interview Exercise
+
+![App Demo](demo/demo.gif)
+
+Hosted on Heroku at url: https://crud-code-test.herokuapp.com/
+
+## Requirements
+
+Create a simple but **impressive** (looks good, works well, has intuitive design, etc.) CRUD application that can do the following:
+
+1. Retrieve employees from a REST API [✔]
+2. Display the employees in a React application [✔]
+3. Has UI mechanisms for creating and deleting employees [✔]
+4. Has API endpoints for creating and deleting employees [✔]
+
+## Bonus (Highly Encouraged)
+
+1. Use a relational database to store the data (SQLite, MariaDB, Postgres) [✔]
+2. UI mechanisms to edit/update employee data [✔]
+3. Add API endpoint to update employee data [✔]
+4. Use [React Table](https://react-table.js.org) [✔]
+
+## Dev Notes:
+
+- Started by moving everything from App to it's individual components
+- Made Employees Component the top level component
+- Loading spinner for when the data becomes larger and the content takes a while to load
+- Updated API endpoints and cleaned up the server code a bit
+- Added some CSS to make it look "better", considered using SCSS but didn't think it was needed for this scale.
+- Right clicking on any of the rows should toggle the assigned.
+- Dark theme option available, also stores which theme you use in the browser localstorage
---
+++ src/components/AddEmployeeModal/AddEmployeeModal.js
@@ -0,0 +1,102 @@
+// Npm Modules
+import React, { Component } from 'react';
+import axios from 'axios';
+import propTypes from 'prop-types';
+import Modal from 'react-awesome-modal';
+
+// Local css imports
+import './AddEmployeeModal.css';
+
+class AddEmployeeModal extends Component {
+  state = {
+    id: '',
+    name: '',
+    code: '',
+    profession: '',
+    city: '',
+    branch: '',
+    color: '',
+  };
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  closeModal = () => {
+    this.props.closeModal();
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    await axios.post(this.props.apiURL('/api/addEmployee'), {
+      id: this.state.id,
+      name: this.state.name,
+      code: this.state.code,
+      profession: this.state.profession,
+      city: this.state.city,
+      branch: this.state.branch,
+      color: this.state.color,
+      assigned: true,
+    });
+
+    setTimeout(() => {
+      this.props.getEmployees();
+      this.props.globalAlerts('Added!');
+      this.props.closeModal();
+    }, 50);
+  };
+
+  renderInputs = () => {
+    const keys = [
+      'id',
+      'name',
+      'code',
+      'profession',
+      'city',
+      'branch',
+      'color',
+    ];
+
+    return keys.map(key => (
+      <div className="shareholder" key={key}>
+        <input
+          type={key === 'id' ? 'number' : 'text'}
+          placeholder={key}
+          name={key}
+          value={this.state[key]}
+          onChange={this.handleInputChange}
+          required
+        />
+      </div>
+    ));
+  };
+
+  render() {
+    return (
+      <section>
+        <Modal visible={true} onClickAway={this.closeModal}>
+          <div>
+            <form id="msform" onSubmit={this.handleSubmit}>
+              <fieldset>{this.renderInputs()}</fieldset>
+              <input
+                type="submit"
+                name="Submit"
+                className="next action-button"
+                value="Submit"
+                required
+              />{' '}
+            </form>
+          </div>
+        </Modal>
+      </section>
+    );
+  }
+}
+
+AddEmployeeModal.propTypes = {
+  closeModal: propTypes.func,
+  getEmployees: propTypes.func,
+  globalAlerts: propTypes.func,
+};
+
+export default AddEmployeeModal;
