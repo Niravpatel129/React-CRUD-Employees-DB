b7631c17d8e4157859c003bc4b4918195ea45e83
Niravpatel129 <niravpatelp129@gmail.com>
2019-12-05 15:36:02 -0500

    refactored server

--- server/index.js
+++ server/index.js
@@ -4,119 +4,17 @@ const app = express();
 
 const mongoose = require("mongoose");
 mongoose.set("useFindAndModify", false);
+app.use(express.json());
 
 const mongooseSetup = require("./mongooseSetup/mongooseSetup.js");
-const employeeSchemaTemplate = require("./Models/employees.model");
+
+const employeesRoutes = require("./routes/employeesRoutes");
 
 mongooseSetup();
 
-const EmployeesDataBase = employeeSchemaTemplate;
-
-let employeess;
-
-function updateEmplyees() {
-  EmployeesDataBase.find(function(err, employees) {
-    if (err) {
-      console.log(err);
-    } else {
-      employeess = employees;
-    }
-  });
-}
-
-updateEmplyees();
-
-app.use(cors());
+app.use("/api", employeesRoutes);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-const corsOptions = {
-  origin: "http://localhost:3000",
-  optionsSuccessStatus: 200
-};
-
-app.get("/api/employees", cors(corsOptions), (req, res, next) => {
-  res.status(200);
-  res.send(JSON.stringify(employeess, null, 2));
-});
-
-app.delete("/api/deleteEmployee", cors(corsOptions), (req, res, next) => {
-  let employeeID = req.query.employeeId;
-
-  EmployeesDataBase.deleteOne({
-    _id: employeeID
-  }).then(() => {
-    updateEmplyees();
-    res.sendStatus(200);
-  });
-});
-
-app.post("/api/addEmployee", cors(corsOptions), (req, res, next) => {
-  const employeeInfo = req.body;
-  const newEmployee = new EmployeesDataBase({
-    id: employeeInfo.id,
-    name: employeeInfo.name,
-    code: employeeInfo.code,
-    profession: employeeInfo.profession,
-    color: employeeInfo.color,
-    city: employeeInfo.city,
-    branch: employeeInfo.branch,
-    assigned: employeeInfo.assigned
-  });
-
-  newEmployee.save(function(err, employee) {
-    if (err) return console.error(err);
-
-    updateEmplyees();
-    res.sendStatus(200);
-    console.log(employee.name + " added to the database!");
-  });
-});
-
-app.put("/api/updateEmployee", cors(corsOptions), (req, res, next) => {
-  const employeeInfo = req.body;
-
-  EmployeesDataBase.findByIdAndUpdate(
-    { _id: employeeInfo._id },
-    {
-      $set: {
-        _id: employeeInfo._id,
-        id: employeeInfo.id,
-        name: employeeInfo.name,
-        code: employeeInfo.code,
-        profession: employeeInfo.profession,
-        city: employeeInfo.city,
-        branch: employeeInfo.branch,
-        color: employeeInfo.color
-      }
-    }
-  )
-    .then(data => {
-      updateEmplyees();
-      res.sendStatus(200);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-app.put("/api/toggleAssigned", cors(corsOptions), (req, res, next) => {
-  const employeeInfo = req.body;
-
-  EmployeesDataBase.findByIdAndUpdate(
-    { _id: employeeInfo._id },
-    {
-      $set: { assigned: req.body.toEnableOrDisable }
-    }
-  )
-    .then(data => {
-      updateEmplyees();
-      res.sendStatus(200);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
 
 app.listen(8080, () => console.log("Job Dispatch API running on port 8080!"));
