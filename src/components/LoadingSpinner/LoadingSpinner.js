48d6d70e403759f8f7d0429dcf35b766986c4070
Niravpatel129 <niravpatelp129@gmail.com>
2019-12-11 13:16:17 -0500

    using functional components

--- src/components/LoadingSpinner/LoadingSpinner.js
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -1,19 +1,17 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 
 import './LoadingSpinner.css';
 
-class LoadingSpinner extends PureComponent {
-  render () {
-    return (
-      <div className='lds-ring'>
-        {/* 4 empty div's are required for the css loading animation */}
-        <div />
-        <div />
-        <div />
-        <div />
-      </div>
-    );
-  }
-}
+const LoadingSpinner = React.memo(() => {
+  return (
+    <div className="lds-ring">
+      {/* 4 empty div's are required for the css loading animation */}
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+});
 
 export default LoadingSpinner;
